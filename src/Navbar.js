6da7d26e5d9a74fbba60080c41b7356eaa55f969
nva12 <nvallee.a7@gmail.com>
2020-02-08 16:06:16 +0400

    add NewPaletteForm

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { withStyles } from "@material-ui/styles";
 import MenuItem from "@material-ui/core/MenuItem";
 import Select from "@material-ui/core/Select";
 import Snackbar from "@material-ui/core/Snackbar";
 import IconButton from "@material-ui/core/IconButton";
 import CloseIcon from "@material-ui/icons/Close";
-import "rc-slider/assets/index.css";
 import Slider from "rc-slider";
-import { withStyles } from "@material-ui/styles";
+import "rc-slider/assets/index.css";
 import styles from "./styles/NavbarStyles";
 
 class Navbar extends Component {
