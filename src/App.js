9417e45618d03e765fe93054cc26ba7af559ea71
nva12 <nvallee.a7@gmail.com>
2020-02-18 15:19:11 +0100

    finish MiniPalette delete

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
     this.state = { palettes: savedPalettes || seedColors };
     this.savePalette = this.savePalette.bind(this);
     this.findPalette = this.findPalette.bind(this);
+    this.deletePalette = this.deletePalette.bind(this);
   }
   findPalette(id) {
     return this.state.palettes.find(function(palette) {
@@ -23,6 +24,14 @@ class App extends Component {
   savePalette(newPalette) {
     this.setState(
       { palettes: [...this.state.palettes, newPalette] },
+      this.syncLocalStorage
+    );
+  }
+  deletePalette(id) {
+    this.setState(
+      st => ({
+        palettes: st.palettes.filter(palette => palette.id !== id)
+      }),
       this.syncLocalStorage
     );
   }
@@ -64,7 +73,11 @@ class App extends Component {
           exact
           path="/"
           render={routeProps => (
-            <PaletteList palettes={this.state.palettes} {...routeProps} />
+            <PaletteList
+              palettes={this.state.palettes}
+              deletePalette={this.deletePalette}
+              {...routeProps}
+            />
           )}
         />
         <Route
